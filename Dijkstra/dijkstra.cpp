890696c4220711a2f300a89ec49852567151b389
rrwrrioe <damir100609@gmail.com>
2025-08-18 21:06:44 +0300

    changes in Graph class for Dijkstra and empty README added

---
+++ Dijkstra/dijkstra.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// this is the simplest, greedy like implementation, and it takes O(N^2)
+
+class Graph {
+	class Node {
+	public:
+		int V;
+		vector<pair<int,int>>parents; // (parent, weight)
+		vector<pair<int,int>>children; // (child, weight)
+		Node(int value = 0) : V(value) {} //value by default; optimized to implement in generics
+
+	};
+
+public:
+	vector<Node>nodes;
+	
+	Graph(const vector<int>& values) {
+		for (int v : values)
+		{
+			nodes.push_back(Node(v));
+		}
+		int n = nodes.size();
+
+		
+	}
+
+	//simple functions of adding nodes and edges
+	void addNode(int V) { nodes.push_back(Node(V)); }
+	void removeNode(int V) {
+		//removal of incident edges
+		for (auto &[child,_] : nodes[V].children)
+		{
+			auto& parents = nodes[child].parents;
+			parents.erase(
+				remove_if(parents.begin(), parents.end(),
+					[&](auto& p) {return p.first == V;}),
+				parents.end()
+			);
+		}
+
+		for (auto& [parent, _] : nodes[V].parents)
+		{
+			auto& children = nodes[parent].children;
+			children.erase(
+				remove_if(children.begin(), children.end(),
+					[&](auto& ch) {return ch.first == V;}),
+				children.end()
+			);
+		}
+
+		//indexes shift after the removal of edges
+		for (auto& node : nodes) {
+			for (auto& child : node.children)
+				if (child.first > V) child.first--;
+			for (auto& parent : node.parents)
+				if (parent.first > V) parent.first--;
+		}
+	}
+	void addEdge(int from, int to, int weight) {  
+		if (from = > nodes.size() || to <= nodes.size()) { return; }
+		nodes[from].children.push_back(to, weight);
+		nodes[to].parents.push_back(from, weight);
+	}
+	void removeEdge(int from, int to) {
+		auto& children = nodes[from].children;
+		children.erase(
+			remove_if(children.begin(), children.end,
+				[&](auto& p) {return p.first == to;}),
+			children(end)
+		);
+
+		auto& parents = nodes[to].parents;
+		parents.erase(
+			remove_if(parents.begin(), parents.end(),
+				[&](auto& p) {return p.first == from;}),
+			parents.end()
+		)
+		//shifts all elements except 'from' to the left, then deletes 'from' Node
+	}
+
+
+	void Dijkstra(int startNode);	// main function
+};
